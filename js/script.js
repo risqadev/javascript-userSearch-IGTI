d0f7d7d9a265afcf87211e7ec3adc06887af66ec
Ricardo Camacho <camacho.rc@gmail.com>
2020-07-25 17:26:21 -0300

    Melhorando o script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
+let allUsers = [];
+let filteredUsers = [];
+
 let searchForm = null;
 let searchInput = null;
 let searchButton = null;
 let tabUsers = null;
 let tabStatistics = null;
-
-let allUsers = [];
-let filteredUsers = [];
 
 async function start() {
   searchForm = document.querySelector('form');
@@ -22,9 +22,10 @@ async function start() {
 start();
 
 async function fetchUsers() {
-  // const res = await fetch(
-  //   'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo'
-  // ); Dados da api copiados uma única vez. Irei usar o arquivo salvo.
+  /* const res = await fetch(
+    'https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo'
+  ); */
+  // Dados da api copiados uma única vez para evitar muitas requisições à API original. Usaremos a partir de agora o arquivo salvo.
   const res = await fetch('./js/apiUsers.json');
   const json = await res.json();
   allUsers = json.results
@@ -60,17 +61,15 @@ const handleFormSubmit = () => {
 
   const search = () => {
     searchValue = searchInput.value.toLowerCase().trim();
-    searchInput.value = '';
+    // searchInput.value = '';
 
-    filteredUsers = allUsers.filter((user) =>
-      user.name.toLowerCase().trim().includes(searchValue)
+    filteredUsers = allUsers.filter(({ name }) =>
+      name.toLowerCase().trim().includes(searchValue)
     );
   };
 
   searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
-
-    // captureSearchValue();
 
     search();
     render();
@@ -78,28 +77,28 @@ const handleFormSubmit = () => {
 };
 
 const render = () => {
-  let countUsers = filteredUsers.length;
-  let countMasc = 0;
-  let countFem = 0;
-  let ageSum = 0;
-  let ageAvg = 0;
+  const countUsers = filteredUsers.length;
+  const countMale = filteredUsers.reduce(
+    (count, { gender }) => (gender === 'male' ? count + 1 : count),
+    0
+  );
+  const countFem = filteredUsers.reduce(
+    (count, { gender }) => (gender === 'female' ? count + 1 : count),
+    0
+  );
+  const ageSum = filteredUsers.reduce(
+    (accululator, { age }) => accululator + age,
+    0
+  );
+  const ageAvg = ageSum / countUsers;
 
   const renderUserList = () => {
     let tabUsersContent = `
-    <h3> ${countUsers} usuário(s) encontrado(s)</h3>
+    <h2> ${countUsers} usuário(s) encontrado(s)</h2>
     <div>
     `;
 
-    filteredUsers.forEach(({ name, photo, age, gender }) => {
-      if (gender === 'male') {
-        countMasc += 1;
-      }
-      if (gender === 'female') {
-        countFem += 1;
-      }
-
-      ageSum += age;
-
+    filteredUsers.forEach(({ name, photo, age }) => {
       const userHTML = `
         <div class="user">
           <img src="${photo}" alt="Foto de ${name}" />
@@ -110,17 +109,15 @@ const render = () => {
       tabUsersContent += userHTML;
     });
 
-    ageAvg = ageSum / countUsers;
-
     tabUsersContent += '</div>';
     tabUsers.innerHTML = tabUsersContent;
   };
 
   const renderStatistics = () => {
     let tabStatisticsContent = `
-      <h3>Estatísticas</h3>
+      <h2>Estatísticas</h2>
       <ul>
-        <li>Sexo masculino: <strong>${formatNumber(countMasc)}</strong></li>
+        <li>Sexo masculino: <strong>${formatNumber(countMale)}</strong></li>
         <li>Sexo feminino: <strong>${formatNumber(countFem)}</strong></li>
         <li>Soma das idades: <strong>${formatNumber(ageSum)}</strong></li>
         <li>Média das idades: <strong>${formatNumber(ageAvg)}</strong></li>
@@ -130,12 +127,12 @@ const render = () => {
     tabStatistics.innerHTML = tabStatisticsContent;
   };
 
+  const formatNumber = (number) => {
+    return Intl.NumberFormat('pt-BR', { maximumFractionDigits: 2 }).format(
+      number
+    );
+  };
+
   renderUserList();
   renderStatistics();
-};
-
-const formatNumber = (number) => {
-  return Intl.NumberFormat('pt-BR', { maximumFractionDigits: 2 }).format(
-    number
-  );
 };
